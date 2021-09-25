83caeaeaf4ea83b8202241efa2d09aa70d7dd6a1
Chloe <chloe.e.muncy@gmail.com>
2021-09-25 16:58:31 -0400

    Added sentiment analysis

--- EmailContentModel/bertweet.cpp
+++ EmailContentModel/bertweet.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include "Python.h"
+#include <stdio.h>
 
 int main() {
-  Py_Initialize();
+  const int PATH_MAX = 1024;
 
-  FILE * berTweetFile = fopen("bertweet.py", "r");
-  if (berTweetFile) {
-    PyRun_SimpleFile(berTweetFile, "bertweet.py");
-    fclose(berTweetFile);
+  FILE* fp;
+
+  int status;
+  char path[PATH_MAX];
+
+  fp = popen("python bertweet.py", "r");
+  if (!fp) {
+    std::cout << "There was an error executing popen()!" << std::endl;
+    return 1;
   }
 
-  Py_Finalize();
+  while (fgets(path, PATH_MAX, fp))
+    std::cout << path << std::endl;
+
+  status = pclose(fp);
+  if (status == -1) {
+    std::cout << "Invalid status code!" << std::endl;
+    return 1;
+  }
 
   return 0;
 }
--- EmailContentModel/bertweet.py
+++ EmailContentModel/bertweet.py
@@ -1,1 +1,17 @@
-print("init")
+from pysentimiento import SentimentAnalyzer, EmotionAnalyzer
+
+
+"""
+Sentiment Analysis
+"""
+analyzer = SentimentAnalyzer(lang="en")
+res = analyzer.predict("i love you")
+print(res.output)
+
+
+"""
+Emotion Analysis
+"""
+# emotion_analyzer = EmotionAnalyzer(lang="en")
+# res = emotion_analyzer.predict("fuck off")
+# print(res.output)
