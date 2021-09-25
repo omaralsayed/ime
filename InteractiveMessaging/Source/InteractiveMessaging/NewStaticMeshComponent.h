5ece1243f38b92673a3b0feef39422f4bc26c542
Soondosm <smullaossman@gmail.com>
2021-09-25 03:02:54 -0400

    C++ component to 1M_Cube

---
+++ InteractiveMessaging/Source/InteractiveMessaging/NewStaticMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "NewStaticMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTIVEMESSAGING_API UNewStaticMeshComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
